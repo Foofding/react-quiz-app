1dc4f3fecfea281089987acac66390004b3120a0
Toby Simcox <tobyrs@hotmail.com>
2022-08-23 17:53:06 -0700

    started logic part. Need to create seperate branch to do this with

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
+import QuestionCard from './Components/QuestionCard';
 import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => {
+
+    const startTrivia = async () => {
+
+    }
+     const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+     }
+
+     const nextQuestion = () => {
+      
+     }
+
+      return (
+      <div className="App">
+      <h1> REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
       <a href="https://youtu.be/F2JCjVSZlG0?t=277">
         <button title="linkButton" style={{border:"1px solid darkgrey"}}> LINK </button>
       </a>
-    </div>
-  );
+      </div>
+      );
+  
+ 
 }
 
 export default App;
---
+++ src/Components/QuestionCard.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const QuestionCard = () => {
+return (
+<div>Question Card</div>
+)}
+
+export default QuestionCard;
